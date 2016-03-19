0603d3fd330bf0ba44d93c808a8ba226dddf6cb4
Connor Peet <connor@peet.io>
2016-03-19 11:27:59 -0400

    Update tetris.proto and add a screen reporter

--- test/unit/frontend/reporter.test.js
+++ test/unit/frontend/reporter.test.js
@@ -18,31 +18,6 @@ describe('reporter', () => {
         clock.restore();
     })
 
-    it('merges reports', () => {
-        expect(reporter.queued.report).to.deep.equal({ joystick: [], tactile: [] });
-
-        reporter.add({});
-        expect(reporter.queued.report).to.deep.equal({ joystick: [], tactile: [] });
-
-        reporter.add({
-            joystick: [{ id: 0, x: 0.5, y: 0.25 }],
-            tactile: [{ id: 2, down: 3, up: 2 }]
-        });
-        expect(reporter.queued.report).to.deep.equal({
-            joystick: [{ id: 0, x: 0.5, y: 0.25 }],
-            tactile: [{ id: 2, down: 3, up: 2 }]
-        });
-
-        reporter.add({
-            joystick: [{ id: 0, x: 0.7, y: 0.45 }, { id: 1, x: 0.3, y: 0.6 }],
-            tactile: [{ id: 2, down: 1, up: 1 }, { id: 3, down: 1, up: 1 }]
-        });
-        expect(reporter.queued.report).to.deep.equal({
-            joystick: [{ id: 0, x: 0.6, y: 0.35 }, { id: 1, x: 0.3, y: 0.6 }],
-            tactile: [{ id: 2, down: 4, up: 3 }, { id: 3, down: 1, up: 1 }]
-        });
-    });
-
     it('sends correctly', () => {
         const c1 = sinon.stub();
         const c2 = sinon.stub();
@@ -50,24 +25,28 @@ describe('reporter', () => {
 
         reporter.add({
             joystick: [{ id: 0, x: 0.5, y: 0.25 }],
-            tactile: [{ id: 2, down: 3, up: 2 }]
+            tactile: [{ id: 2, down: 3, up: 2 }],
+            screen: [{ id: 3, x: 0.1, y: 0.5, clicks: 1 }],
         }, c1);
         clock.tick(51);
         expect(c1.called).to.be.true;
         expect(mock.send.args.length).to.equal(1);
         expect(mock.send.args[0][0].props).to.deep.equal({
             joystick: [{ id: 0, x: 0.5, y: 0.25 }],
-            tactile: [{ id: 2, down: 3, up: 2 }]
+            tactile: [{ id: 2, down: 3, up: 2 }],
+            screen: [{ id: 3, x: 0.1, y: 0.5, clicks: 1 }],
         });
 
         reporter.add({
             joystick: [{ id: 0, x: 0.5, y: 0.25 }],
-            tactile: [{ id: 2, down: 3, up: 2 }]
+            tactile: [{ id: 2, down: 3, up: 2 }],
+            screen: [{ id: 3, x: 0.1, y: 0.5 }, { id: 4, clicks: 1 }],
         }, c2);
         clock.tick(20);
         reporter.add({
             joystick: [{ id: 0, x: 0.7, y: 0.45 }, { id: 1, x: 0.3, y: 0.6 }],
-            tactile: [{ id: 2, down: 1, up: 1 }, { id: 3, down: 1, up: 1 }]
+            tactile: [{ id: 2, down: 1, up: 1 }, { id: 3, down: 1, up: 1 }],
+            screen: [{ id: 3, clicks: 1 }, { id: 4, x: 0.3, y: 0.3, clicks: 2 }],
         }, c3);
         clock.tick(31);
 
@@ -76,7 +55,8 @@ describe('reporter', () => {
         expect(mock.send.args.length).to.equal(2);
         expect(mock.send.args[1][0].props).to.deep.equal({
             joystick: [{ id: 0, x: 0.6, y: 0.35 }, { id: 1, x: 0.3, y: 0.6 }],
-            tactile: [{ id: 2, down: 4, up: 3 }, { id: 3, down: 1, up: 1 }]
+            tactile: [{ id: 2, down: 4, up: 3 }, { id: 3, down: 1, up: 1 }],
+            screen: [{ id: 3, x: 0.1, y: 0.5, clicks: 1 }, { id: 4, x: 0.3, y: 0.3, clicks: 3 }],
         });
     });
 
@@ -99,14 +79,11 @@ describe('reporter', () => {
 
         clock.tick(51);
         expect(mock.send.args.length).to.equal(1);
-        expect(mock.send.args[0][0].props).to.deep.equal({
-            joystick: [],
-            tactile: [
-                { id: 1, down: 1 },
-                { id: 2, up: 1 },
-                { id: 3, down: 1, up: 1 },
-                { id: 4, up: 2, down: 0 },
-            ]
-        });
+        expect(mock.send.args[0][0].props.tactile).to.deep.equal([
+            { id: 1, down: 1 },
+            { id: 2, up: 1 },
+            { id: 3, down: 1, up: 1 },
+            { id: 4, up: 2, down: 0 },
+        ]);
     });
 });
