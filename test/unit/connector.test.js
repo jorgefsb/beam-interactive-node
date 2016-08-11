57ff7a300cfec980c740aa98122d3b5d257a686a
Simon Schick <demwizzy@gmail.com>
2016-08-11 04:47:03 +0200

    re-add tests, add more events

--- test/unit/connector.test.js
+++ test/unit/connector.test.js
@@ -1,5 +1,6 @@
 import Connector from '../../lib/connector';
 import { expect } from 'chai';
+import { EventEmitter } from 'events';
 
 describe('connector', () => {
     it('bubbles error correctly', () => {
@@ -13,5 +14,28 @@ describe('connector', () => {
         c.setOpen(false);
         c.bubbleOpen('error', 'asdf');
         expect(i).to.equal(1);
+    });
+
+    it('emits a disconnect event when the socket disconnects', callback => {
+        const c = new Connector('');
+        const emitter = new EventEmitter();
+        c.connect(() => {}, () => emitter);
+        c.on('disconnect', callback);
+        emitter.emit('close');
+    });
+
+    it('emits a reconnect event after disconnecting', callback => {
+        const c = new Connector('');
+        const emitter = new EventEmitter();
+        c.connect(() => {}, () => {
+            setTimeout(() => {
+                emitter.emit('open');
+            }, 100);
+            return emitter;
+        });
+        c.reconnectAttempts = -1;
+        emitter.emit('close');
+        c.on('disconnect', callback);
+        c.on('reconnect', callback);
     });
 });
